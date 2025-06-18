a8bc075299076e032d137d07fa6a2cd7f1d5d256
abdallah-awwad <abdallahhawwad@gmail.com>
2025-06-18 03:35:36 +0300

    v1.0.0: Initial release

---
+++ src/config.js
@@ -0,0 +1,5 @@
+// Configuration - Edit these values to customize the extension
+export const collection = "your_collection"; // The collection to validate
+export const firstDateField = "start_date"; // First date field name
+export const secondDateField = "end_date"; // Second date field name
+export const errorMessage = '"End Date" must be greater than "Start Date"'; // Custom error message
